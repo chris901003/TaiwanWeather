0b3a873e21b80d1e72128cd511c428650745546a
chris901003 <a0987999103@gmail.com>
2023-03-19 15:03:56 +0800

    Initial Commit

---
+++ TaiwanWeatherWidget/TaiwanWeatherWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaiwanWeatherWidgetApp.swift
+//  TaiwanWeatherWidget
+//
+//  Created by 黃弘諺 on 2023/3/19.
+//
+
+import SwiftUI
+
+@main
+struct TaiwanWeatherWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
